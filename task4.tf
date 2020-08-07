2d885c37bba2c5a40e02c1bb20a0657d5fc0411e
Nilesh Mathur <64744992+nileshmathur@users.noreply.github.com>
2020-08-07 23:25:04 +0530

    Update task4.tf

--- task4.tf
+++ task4.tf
@@ -82,6 +82,22 @@ resource "aws_nat_gateway" "nilesh_ng" {
 
 
 ############################################################################################################################################
+resource "aws_route_table" "nilesh_nat" {
+	vpc_id=aws_vpc.nilesh_vpc.id
+	route{
+		cidr_block="0.0.0.0/0"
+		gateway_id 		= aws_nat_gateway.nilesh_ng.id                 #Creating Routing Table and Associating it with respective subnet
+
+	}
+}
+resource "aws_route_table_association" "pri_subnet_rt" {
+	depends_on=[aws_route_table.nilesh_nat]
+	subnet_id=aws_subnet.pri_subnet.id
+	route_table_id=aws_route_table.nilesh_nat.id
+	
+}
+
+#######################################################################################################################################3
 resource "tls_private_key" "key_pair" {
 	algorithm 			= "RSA"
 }
